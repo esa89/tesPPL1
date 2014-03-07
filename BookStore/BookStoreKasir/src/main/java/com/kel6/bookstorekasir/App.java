dd8f3a5e99671033f4cd0e25914cbdde67e448fc
esa89 <esa.fauzi@gmail.com>
2014-03-07 16:20:16 +0700

    Create Projek pertama

---
+++ BookStore/BookStoreKasir/src/main/java/com/kel6/bookstorekasir/App.java
@@ -0,0 +1,13 @@
+package com.kel6.bookstorekasir;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
